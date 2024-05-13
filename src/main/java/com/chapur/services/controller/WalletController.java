d7fa942b705622a5fdfaeed7941d7a5b6989aea3
jcardenas <jcardenas@imasd.com.mx>
2024-05-13 12:40:48 -0600

    Agrega comentario javadoc a controlador de wallet

--- src/main/java/com/chapur/services/controller/WalletController.java
+++ src/main/java/com/chapur/services/controller/WalletController.java
@@ -26,6 +26,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * The Wallet controller.
+ */
 @RestController
 @RequestMapping("/wallet")
 @Slf4j
