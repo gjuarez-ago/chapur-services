7357518f2e27a6a0633f7bd53e0cd322534dbc0e
Diego Hernandez <dhernandez@imasd.com.mx>
2024-05-13 18:05:54 -0400

    crytp added

---
+++ src/main/java/com/chapur/services/models/DecryptDTO.java
@@ -0,0 +1,14 @@
+package com.chapur.services.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class DecryptDTO {
+
+    private String keyword;
+
+}
